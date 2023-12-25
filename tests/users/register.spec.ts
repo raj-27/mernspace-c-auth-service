b1efb48eef7b84185216c282c2c8e9b056860f94
Raj <raj819314@gmail.com>
2023-12-25 14:57:00 +0530

    Add testing

---
+++ tests/users/register.spec.ts
@@ -0,0 +1,43 @@
+import request from "supertest";
+import app from "../../src/app";
+
+describe("POST /auth/register", () => {
+    describe("Given all field", () => {
+        it("Should return the 201 status code", async () => {
+            // Arrange
+            const userData = {
+                firstname: "Raj",
+                lastName: "Yadav",
+                email: "raj819314@gmail.com",
+                password: "secret",
+            };
+            // Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            // Assert
+            expect(response.statusCode).toBe(201);
+        });
+
+        it("It should return valid json", async () => {
+            // Arrange
+            const userData = {
+                firstname: "Raj",
+                lastName: "Yadav",
+                email: "raj819314@gmail.com",
+                password: "secret",
+            };
+            // Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            // Assert
+            expect(response.headers["content-type"]).toEqual(
+                expect.stringContaining("json"),
+            );
+        });
+    });
+    describe("Fields are missing", () => {});
+});
