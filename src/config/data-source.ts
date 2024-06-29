afa9c707df64e56cb12ddb71cb4922baf3705d1f
Raj <raj819314@gmail.com>
2024-06-29 11:13:03 +0530

    add initial migraton

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -14,6 +14,6 @@ export const AppDataSource = new DataSource({
     synchronize: false,
     logging: false,
     entities: [User, RefreshToken],
-    migrations: [],
+    migrations: ["src/migration/*.ts"],
     subscribers: [],
 });
