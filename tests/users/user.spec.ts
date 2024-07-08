60ab22371ea0ef7cde5354c84cf6d28bb47c2215
Raj <raj819314@gmail.com>
2024-07-08 21:40:10 +0530

    Refactor user test cases

--- tests/users/user.spec.ts
+++ tests/users/user.spec.ts
@@ -1,8 +1,9 @@
-import request from "supertest";
 import { DataSource } from "typeorm";
+import request from "supertest";
+import createJWKSMock from "mock-jwks";
+
 import { AppDataSource } from "../../src/config/data-source";
 import app from "../../src/app";
-import createJWKSMock from "mock-jwks";
 import { Roles } from "../../src/constants";
 import { User } from "../../src/entity";
 
@@ -21,7 +22,7 @@ describe("GET /auth/self", () => {
         await connection.synchronize();
     });
 
-    afterEach(async () => {
+    afterEach(() => {
         jwks.stop();
     });
 
@@ -29,7 +30,7 @@ describe("GET /auth/self", () => {
         await connection.destroy();
     });
 
-    describe("Given All Fields", () => {
+    describe("Given all fields", () => {
         it("should return the 200 status code", async () => {
             const accessToken = jwks.token({
                 sub: "1",
@@ -45,10 +46,10 @@ describe("GET /auth/self", () => {
         it("should return the user data", async () => {
             // Register user
             const userData = {
-                firstName: "John",
-                lastName: "Doe",
-                email: "johndoe12@gmail.com",
-                password: "secret",
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "password",
             };
             const userRepository = connection.getRepository(User);
             const data = await userRepository.save({
@@ -70,13 +71,14 @@ describe("GET /auth/self", () => {
             // Check if user id matches with registered user
             expect((response.body as Record<string, string>).id).toBe(data.id);
         });
-        it("should not contain password field", async () => {
+
+        it("should not return the password field", async () => {
             // Register user
             const userData = {
-                firstName: "John",
-                lastName: "Doe",
-                email: "johndoe12@gmail.com",
-                password: "secret",
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "password",
             };
             const userRepository = connection.getRepository(User);
             const data = await userRepository.save({
@@ -96,9 +98,29 @@ describe("GET /auth/self", () => {
                 .send();
             // Assert
             // Check if user id matches with registered user
-            expect(
-                (response.body as Record<string, string>).id,
-            ).not.toHaveProperty("password");
+            expect(response.body as Record<string, string>).not.toHaveProperty(
+                "password",
+            );
+        });
+
+        it("should return 401 status code if token does not exists", async () => {
+            // Register user
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "password",
+            };
+            const userRepository = connection.getRepository(User);
+            await userRepository.save({
+                ...userData,
+                role: Roles.CUSTOMER,
+            });
+
+            // Add token to cookie
+            const response = await request(app).get("/auth/self").send();
+            // Assert
+            expect(response.statusCode).toBe(401);
         });
     });
 });
