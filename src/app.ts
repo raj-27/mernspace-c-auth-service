1eb5dfb659c43cce331a82033dd6106964de7855
Raj <raj819314@gmail.com>
2024-07-14 12:54:55 +0530

    update cors options

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["http://localhost:5173/"],
+        origin: ["http://localhost:5173"],
+        methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
         credentials: true,
     }),
 );
