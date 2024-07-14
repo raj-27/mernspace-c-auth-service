b079fa83adecceb7d3a5c7b718209b11ba83eb50
Raj <raj819314@gmail.com>
2024-07-14 13:14:55 +0530

    exporting listtenantvalidator

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,3 +1,4 @@
 export { default as loginValidator } from "./login-validator";
 export { default as registerValidator } from "./register-validator";
 export { default as tenantValidator } from "./tenantValidator";
+export { default as listTenantValidator } from "./list-tenant-validator";
