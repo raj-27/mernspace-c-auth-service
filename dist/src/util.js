fa166509fc33a902dc59e547a507c7f18d931b27
Raj Yadav <raj819314@gmail.com>
2025-06-08 11:05:15 +0530

    remove build folder

--- dist/src/util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateDiscount = void 0;
-const calculateDiscount = (price, percentage) => {
-    return price * (percentage / 100);
-};
-exports.calculateDiscount = calculateDiscount;
